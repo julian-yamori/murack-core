26fefd2f7ee68a54a6e4d3bd74f721c24af30568
julian-yamori <julian.lampyridae@gmail.com>
2025-07-21 16:58:19 +0000

    assert_eq_not_orderd() の失敗時に説明文を追加

--- domain/src/test_utils.rs
+++ domain/src/test_utils.rs
@@ -1,7 +1,22 @@
+use std::fmt::Display;
+
 //順番不問でスライス内容の一致を確認
-pub fn assert_eq_not_orderd<T: PartialEq>(left: &[T], right: &[T]) {
-    assert_eq!(left.len(), right.len());
+pub fn assert_eq_not_orderd<T>(left: &[T], right: &[T])
+where
+    T: PartialEq + Display,
+{
+    assert_eq!(
+        left.len(),
+        right.len(),
+        "assert_eq_not_orderd : スライスの長さが一致するか確認 : {}, {}",
+        left.len(),
+        right.len()
+    );
+
     for path in right {
-        assert!(left.iter().any(|p| p == path));
+        assert!(
+            left.iter().any(|p| p == path),
+            "assert_eq_not_orderd : left に {path} が含まれるか確認"
+        );
     }
 }
