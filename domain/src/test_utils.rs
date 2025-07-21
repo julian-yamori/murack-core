0ad17aaecccf9de3560eed0267e679644559f3a1
julian-yamori <julian.lampyridae@gmail.com>
2025-07-21 17:33:15 +0000

    assert_eq_not_orderd の修正
    - assert_eq_not_orderd 自体のテストの追加
    - should_panics::item_count_not_matches のケースで panic しなかった問題を修正

--- domain/src/test_utils.rs
+++ domain/src/test_utils.rs
@@ -13,10 +13,60 @@ where
         right.len()
     );
 
-    for path in right {
-        assert!(
-            left.iter().any(|p| p == path),
-            "assert_eq_not_orderd : left に {path} が含まれるか確認"
+    for item in right {
+        assert_eq!(
+            left.iter().filter(|i| *i == item).count(),
+            right.iter().filter(|i| *i == item).count(),
+            "assert_eq_not_orderd : {item} の数が一致するか確認"
         );
+    }
+}
+
+#[cfg(test)]
+mod assert_eq_not_orderd_tests {
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case(&[1,2,3], &[1,2,3])]
+    #[test_case(&[1,2,3], &[2,1,3])]
+    #[test_case(&[], &[])]
+    #[test_case(&[1], &[1])]
+    fn should_success(left: &[i32], right: &[i32]) {
+        assert_eq_not_orderd(left, right)
+    }
+
+    mod should_panics {
+        use super::*;
+
+        #[test]
+        #[should_panic]
+        fn length_not_matches() {
+            assert_eq_not_orderd(&[1, 2, 3], &[1, 2])
+        }
+
+        #[test]
+        #[should_panic]
+        fn right_is_empty() {
+            assert_eq_not_orderd(&[1, 2, 3], &[])
+        }
+
+        #[test]
+        #[should_panic]
+        fn left_is_empty() {
+            assert_eq_not_orderd(&[], &[1, 2, 3])
+        }
+
+        #[test]
+        #[should_panic]
+        fn item_not_equals() {
+            assert_eq_not_orderd(&[1, 2, 3], &[1, 99, 3])
+        }
+
+        #[test]
+        #[should_panic]
+        fn item_count_not_matches() {
+            assert_eq_not_orderd(&[1, 1, 1, 2, 2], &[1, 1, 2, 2, 2])
+        }
     }
 }
