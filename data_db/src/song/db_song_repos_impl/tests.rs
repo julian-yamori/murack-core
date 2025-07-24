e2e7810a2b7a276e3e9a8ac69c34c22caa3ee1d8
julian-yamori <julian.lampyridae@gmail.com>
2025-07-24 16:43:28 +0000

    `SongDao::select_path_begins_directory` のテストを `DbSongRepositoryImpl::get_path_by_directory`のテストとして移行 #20

--- data_db/src/song/db_song_repos_impl/tests.rs
+++ data_db/src/song/db_song_repos_impl/tests.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use murack_core_domain::{
     db::DbTransaction,
-    path::{LibPathStr, LibSongPath},
+    path::{LibDirPath, LibPathStr, LibSongPath},
     song::DbSongRepository,
 };
 use sqlx::PgPool;
@@ -125,6 +125,110 @@ mod test_get_path_by_path_str {
             .fetch_one(&mut **tx.get())
             .await?;
         assert_eq!(total_count, 3);
+
+        Ok(())
+    }
+}
+
+// get_path_by_directory 関数のテスト
+mod test_get_path_by_directory {
+    use super::*;
+
+    #[sqlx::test(
+        migrator = "crate::MIGRATOR",
+        fixtures("fixtures/test_get_path_by_directory/normal_chars.sql")
+    )]
+    fn 浅いディレクトリを指定(pool: PgPool) -> anyhow::Result<()> {
+        let song_dao = SongDaoImpl {};
+        let target = DbSongRepositoryImpl::new(song_dao);
+
+        let mut tx = DbTransaction::PgTransaction {
+            tx: pool.begin().await?,
+        };
+
+        assert_eq!(
+            target
+                .get_path_by_directory(&mut tx, &LibDirPath::new("test"))
+                .await?,
+            vec![
+                LibSongPath::new("test/hoge.flac"),
+                LibSongPath::new("test/hoge2.flac"),
+                LibSongPath::new("test/dir/hoge3.flac"),
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[sqlx::test(
+        migrator = "crate::MIGRATOR",
+        fixtures("fixtures/test_get_path_by_directory/normal_chars.sql")
+    )]
+    fn 少し深いディレクトリを指定(pool: PgPool) -> anyhow::Result<()> {
+        let song_dao = SongDaoImpl {};
+        let target = DbSongRepositoryImpl::new(song_dao);
+
+        let mut tx = DbTransaction::PgTransaction {
+            tx: pool.begin().await?,
+        };
+
+        assert_eq!(
+            target
+                .get_path_by_directory(&mut tx, &LibDirPath::new("test/dir"))
+                .await?,
+            vec![LibSongPath::new("test/dir/hoge3.flac"),]
+        );
+
+        Ok(())
+    }
+
+    #[sqlx::test(
+        migrator = "crate::MIGRATOR",
+        fixtures("fixtures/test_get_path_by_directory/normal_chars.sql")
+    )]
+    fn ルート指定(pool: PgPool) -> anyhow::Result<()> {
+        let song_dao = SongDaoImpl {};
+        let target = DbSongRepositoryImpl::new(song_dao);
+
+        let mut tx = DbTransaction::PgTransaction {
+            tx: pool.begin().await?,
+        };
+
+        assert_eq!(
+            target
+                .get_path_by_directory(&mut tx, &LibDirPath::root())
+                .await?,
+            vec![
+                LibSongPath::new("test/hoge.flac"),
+                LibSongPath::new("test/hoge2.flac"),
+                LibSongPath::new("fuga.flac"),
+                LibSongPath::new("dummy/fuga.flac"),
+                LibSongPath::new("test/dir/hoge3.flac"),
+                LibSongPath::new("dummy/test/dir/dummy.mp3"),
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[sqlx::test(
+        migrator = "crate::MIGRATOR",
+        fixtures("fixtures/test_get_path_by_directory/special_chars.sql")
+    )]
+    fn 特殊文字を挟むパスでの検索(pool: PgPool) -> anyhow::Result<()> {
+        let song_dao = SongDaoImpl {};
+        let target = DbSongRepositoryImpl::new(song_dao);
+
+        let mut tx = DbTransaction::PgTransaction {
+            tx: pool.begin().await?,
+        };
+
+        assert_eq!(
+            target
+                .get_path_by_directory(&mut tx, &LibDirPath::new("test/d%i_r$"))
+                .await?,
+            vec![LibSongPath::new("test/d%i_r$/hoge.flac"),]
+        );
 
         Ok(())
     }
