8d57610b358b94c7406cf16ca32ef85faef9a757
julian-yamori <julian.lampyridae@gmail.com>
2025-07-28 17:41:15 +0000

    DbPlaylistTrackRepository::delete_track_from_all_playlists で無駄に全カラム取得していたのを修正

--- domain/src/playlist/db_playlist_track_repos_impl.rs
+++ domain/src/playlist/db_playlist_track_repos_impl.rs
@@ -2,10 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use sqlx::PgTransaction;
 
-use crate::{
-    NonEmptyString,
-    playlist::{DbPlaylistTrackRepository, PlaylistType, SortType, playlist_row::PlaylistRow},
-};
+use crate::playlist::DbPlaylistTrackRepository;
 
 use super::playlist_sqls;
 
@@ -21,22 +18,21 @@ impl DbPlaylistTrackRepository for DbPlaylistTrackRepositoryImpl {
         tx: &mut PgTransaction<'c>,
         track_id: i32,
     ) -> Result<()> {
-        let playlist_rows = sqlx::query_as!(PlaylistRow, r#"SELECT id, playlist_type AS "playlist_type: PlaylistType", name AS "name: NonEmptyString", parent_id, in_folder_order, filter_json, sort_type AS "sort_type: SortType", sort_desc, save_dap ,listuped_flag ,dap_changed FROM playlists"#)
+        // 全てのプレイリストについて処理
+        let playlist_ids = sqlx::query_scalar!(r#"SELECT id FROM playlists"#)
             .fetch_all(&mut **tx)
             .await?;
-
-        //全てのプレイリストについて繰り返す
-        for plist in playlist_rows {
+        for playlist_id in playlist_ids {
             //プレイリスト内の曲を取得
-            let tracks = playlist_sqls::select_track_id_by_playlist_id(tx, plist.id).await?;
+            let tracks = playlist_sqls::select_track_id_by_playlist_id(tx, playlist_id).await?;
 
             //プレイリストから一旦全削除
-            playlist_sqls::delete_by_playlist_id(tx, plist.id).await?;
+            playlist_sqls::delete_by_playlist_id(tx, playlist_id).await?;
 
             //削除対象の曲を除き、全て追加
             let add_tracks = tracks.iter().filter(|i| **i != track_id).enumerate();
             for (order, it) in add_tracks {
-                playlist_sqls::insert_playlist_track(tx, plist.id, *it, order as i32).await?;
+                playlist_sqls::insert_playlist_track(tx, playlist_id, *it, order as i32).await?;
             }
         }
 
