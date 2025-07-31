223ee4e1dab89a473c59a6551ee87c403e23a61d
julian-yamori <julian.lampyridae@gmail.com>
2025-07-31 19:53:53 +0000

    test_get_whole_tree_treed.sql の infolder_order を、UNIQUE 制約を満たすよう修正

--- domain/src/playlist/playlist_tree/fixtures/test_get_whole_tree_treed.sql
+++ domain/src/playlist/playlist_tree/fixtures/test_get_whole_tree_treed.sql
@@ -14,8 +14,8 @@ VALUES
     (3, 'normal', '1-1', 5, 0, 'artist', false, true, false, true),
     (5, 'normal', 'root1', NULL, 0, 'artist', false, true, false, true),
     (9, 'normal', '1-2-1', 2, 0, 'artist', false, true, false, true),
-    (2, 'normal', '1-2', 5, 0, 'artist', false, true, false, true),
-    (35, 'normal', 'root2', NULL, 0, 'artist', false, true, false, true),
+    (2, 'normal', '1-2', 5, 1, 'artist', false, true, false, true),
+    (35, 'normal', 'root2', NULL, 1, 'artist', false, true, false, true),
     (75, 'normal', '2-1', 35, 0, 'artist', false, true, false, true),
-    (98, 'normal', '1-2-2', 2, 0, 'artist', false, true, false, true),
-    (1, 'normal', '1-3', 5, 0, 'artist', false, true, false, true);
+    (98, 'normal', '1-2-2', 2, 1, 'artist', false, true, false, true),
+    (1, 'normal', '1-3', 5, 2, 'artist', false, true, false, true);
