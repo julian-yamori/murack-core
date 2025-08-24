a4b422c7cae45970e62751299b2ac2d0607657ba
julian-yamori <julian.lampyridae@gmail.com>
2025-08-24 06:33:35 +0000

    playlist_sqls::set_dap_changed を追加

--- domain/src/playlist/playlist_sqls.rs
+++ domain/src/playlist/playlist_sqls.rs
@@ -16,6 +16,22 @@ pub async fn reset_listuped_flag<'c>(tx: &mut PgTransaction<'c>) -> sqlx::Result
     Ok(())
 }
 
+pub async fn set_dap_changed<'c>(
+    tx: &mut PgTransaction<'c>,
+    playlist_id: i32,
+    is_changed: bool,
+) -> sqlx::Result<()> {
+    sqlx::query!(
+        "UPDATE playlists SET dap_changed = $1 WHERE id = $2",
+        is_changed,
+        playlist_id
+    )
+    .execute(&mut **tx)
+    .await?;
+
+    Ok(())
+}
+
 /// 全プレイリストの、DAPに保存してからの変更フラグを設定
 /// # Arguments
 /// - is_changed: 変更されたか
